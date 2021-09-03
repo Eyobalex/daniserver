b6f827da72d15cf8a277a77669cf7360ecc043ae
Eyob alex <eyobalemayehu0@gmail.com>
2021-09-03 20:15:21 +0300

    idfj

--- controllers/admin/categories.js
+++ controllers/admin/categories.js
@@ -13,15 +13,32 @@ export const getCategories= async (req, res) => {
 
 export const createCategories = async (req, res) => {
     try {
-        if(!req.userId){
-            res.status(403).json({message: "you need to login"});
-            return;
-        } 
-        const {category, faicon} = req.body;
-        const newCategory = await Category.create({name: category, fa_icon: faicon});
+        // if(!req.userId){
+        //     res.status(403).json({message: "you need to login"});
+        //     return;
+        // } 
+        const {name, faicon} = req.body;
+        const newCategory = await Category.create({name, fa_icon: faicon});
 
         res.status(201).json(newCategory)
         
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const editCategories= async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {name, faicon} =req.body;
+
+        // console.log( req.body );
+        if(! mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({message: "category id is not valid"});
+
+        const oldCategory = await Category.findByIdAndUpdate({_id: id}, {name, fa_icon: faicon}, {new: true});
+        res.status(202).json(oldCategory);
+
+
     } catch (error) {
         console.error(error);
     }
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCategories, deleteCategories, getCategories } from '../controllers/admin/categories.js';
+import { createCategories, deleteCategories, getCategories, editCategories } from '../controllers/admin/categories.js';
 import {getUsers, deleteUsers} from '../controllers/admin/users.js';
 import auth from '../middleware/auth.js'
 import {admin} from '../middleware/role.js'
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/users',  getUsers);
 router.get('/category', getCategories);
 router.post('/category',createCategories);
+router.patch('/category/:id',editCategories);
 
 router.delete('/category/:id',deleteCategories);
 router.delete('/users/:id', deleteUsers);
